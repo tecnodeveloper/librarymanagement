b2875c2b37117078d4f4a0f66ee477d75972852e
Zain Ali <recluzedev@gmail.com>
2026-01-10 19:55:43 +0500

    Library Management system using laravel

---
+++ documentation.md
@@ -0,0 +1,11 @@
+# Library Management System
+
+## Main Points
+
+- User can understand these Concepts after completing this library management system
+
+1. Laravel (Routing, Elqouent ORM, Models, Controllers).
+2. OOP
+3. MySql
+4. Starter Kit (React of laravel)
+5. API & Postman for testing
---
+++ node_modules/@inertiajs/react/types/Form.d.ts
@@ -0,0 +1,19 @@
+import { FormComponentSlotProps, FormDataConvertible, Method } from '@inertiajs/core';
+import React, { ReactNode } from 'react';
+declare const Form: React.ForwardRefExoticComponent<Partial<Pick<import("@inertiajs/core").Visit<import("@inertiajs/core").RequestPayload>, "headers" | "errorBag" | "queryStringArrayFormat" | "showProgress" | "invalidateCacheTags"> & Omit<import("@inertiajs/core").VisitCallbacks<import("@inertiajs/core").RequestPayload>, "onPrefetched" | "onPrefetching">> & {
+    method?: Method | Uppercase<Method>;
+    action?: string | import("@inertiajs/core").UrlMethodPair;
+    transform?: (data: Record<string, FormDataConvertible>) => Record<string, FormDataConvertible>;
+    options?: import("@inertiajs/core").FormComponentOptions;
+    onSubmitComplete?: (props: import("@inertiajs/core").FormComponentonSubmitCompleteArguments) => void;
+    disableWhileProcessing?: boolean;
+    resetOnSuccess?: boolean | string[];
+    resetOnError?: boolean | string[];
+    setDefaultsOnSuccess?: boolean;
+    validateFiles?: boolean;
+    validationTimeout?: number;
+    withAllErrors?: boolean;
+} & Omit<React.FormHTMLAttributes<HTMLFormElement>, "children" | "method" | "headers" | "errorBag" | "queryStringArrayFormat" | "showProgress" | "invalidateCacheTags" | "onCancelToken" | "onBefore" | "onBeforeUpdate" | "onStart" | "onProgress" | "onFinish" | "onCancel" | "onSuccess" | "onError" | "onFlash" | "options" | "action" | "transform" | "onSubmitComplete" | "disableWhileProcessing" | "resetOnSuccess" | "resetOnError" | "setDefaultsOnSuccess" | "validateFiles" | "validationTimeout" | "withAllErrors"> & Omit<React.AllHTMLAttributes<HTMLFormElement>, "children" | "method" | "headers" | "errorBag" | "queryStringArrayFormat" | "showProgress" | "invalidateCacheTags" | "onCancelToken" | "onBefore" | "onBeforeUpdate" | "onStart" | "onProgress" | "onFinish" | "onCancel" | "onSuccess" | "onError" | "onFlash" | "options" | "action" | "transform" | "onSubmitComplete" | "disableWhileProcessing" | "resetOnSuccess" | "resetOnError" | "setDefaultsOnSuccess" | "validateFiles" | "validationTimeout" | "withAllErrors"> & {
+    children: ReactNode | ((props: FormComponentSlotProps) => ReactNode);
+} & React.RefAttributes<FormComponentSlotProps>>;
+export default Form;
