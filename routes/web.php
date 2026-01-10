eea14cee46794dc4524c6ca46463dfc59d5f74a4
Zain Ali <recluzedev@gmail.com>
2026-01-10 21:52:26 +0500

    remove the defualt file app.blade.php file

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,13 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\WorkOS\Http\Middleware\ValidateSessionWithWorkOS;
 
-Route::get('/', function () {
-    return Inertia::render('welcome');
+
+Route::get('/',function(){
+    return view('dashboard');
 })->name('home');
+// Route::get('/', function () {
+//     return Inertia::render('dashboard');
+// })
 
 Route::middleware([
     'auth',
